cbf6c8d64b5f421f6beb06e11a4624ab104fc7ac
sagralibri <donovinxbyrd@gmail.com>
2025-03-12 11:20:23 -0400

    some minor things..

--- test/Assets/BattleAI.cs
+++ test/Assets/BattleAI.cs
@@ -6,7 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class BattleAI : MonoBehaviour, BattleHandler
+public class BattleAI : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
